987611d09c321e81195c2286502a27c4a5813ea8
mutian <bhfqlz@qq.com>
2022-10-15 21:32:18 +0800

    notices:
    version 1.0.5

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'muyomu/http',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '26d14d73f0de7e1b30716742919363ad29b57df1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'muyomu/database' => array(
+            'pretty_version' => 'v1.0.3',
+            'version' => '1.0.3.0',
+            'reference' => '7e4e7b94f8668d53400e3873add5ee4afe554c15',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../muyomu/database',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'muyomu/http' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '26d14d73f0de7e1b30716742919363ad29b57df1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
